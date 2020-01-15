b5a63bc3b0e00e68bce450d1d79f281fc957e0f0
local <local@WMII-V6C52G01FU>
2020-01-15 13:31:31 +0100

    Dodany S80Builder

---
+++ Lekcja13/S80Builder.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lekcja13
+{
+    class S80Builder : IBuilder
+    {
+        private Car _car = new VolvoS80();
+
+        public void AddEngine()
+        {
+            _car.Engine = "Diesel T4";
+        }
+
+        public void AddEquipment()
+        {
+            List<string> equipmentList = new List<string>();
+            equipmentList.Add("Automatyczna klimatyzacja");
+            equipmentList.Add("Zwykłe radio");
+            equipmentList.Add("Komputer pokładowy");
+            _car.Equipment = equipmentList;
+        }
+
+        public void AddSeats()
+        {
+            _car.Seats = "Szare, podgrzewane fotele";
+        }
+
+        public void AddWheels()
+        {
+            _car.Wheels = "Klasyczne, stalowe felgi";
+        }
+
+        public Car GetCar()
+        {
+            return _car;
+        }
+    }
+}
---
+++ Lekcja13/VolvoS80.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lekcja13
+{
+    class VolvoS80 : Car
+    {
+        public VolvoS80()
+        {
+            Name = "Volvo";
+            Model = "S80";
+        }
+
+    }
+}
